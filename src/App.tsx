904328001532e614dcba6c1ca7998c3faa158f66
Daniel <carreraprogrammer@gmail.com>
2024-01-02 19:01:02 -0500

    Refactor App component to include authentication and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,35 @@
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
+import { IonApp } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+import { useState } from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import AppTabs from './AppTabs';
+import { AuthContext } from './auth';
+import LoginPage from './pages/LoginPage';
+import NotFoundPage from './pages/NotFoundPage';
 
-/* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
-
-/* Theme variables */
-import './theme/variables.css';
-
-setupIonicReact();
-
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  console.log(`rendering App with loggedIn=${loggedIn}`);
+  return (
+    <IonApp>
+      <AuthContext.Provider value={{ loggedIn }}>
+        <IonReactRouter>
+          <Switch>
+            <Route exact path="/login">
+              <LoginPage onLogin={() => setLoggedIn(true)} />
+            </Route>
+            <Route path="/my">
+              <AppTabs />
+            </Route>
+            <Redirect exact path="/" to="/my/entries" />
+            <Route>
+              <NotFoundPage />
+            </Route>
+          </Switch>
+        </IonReactRouter>
+      </AuthContext.Provider>
+    </IonApp>
+  );
+};
 
 export default App;
